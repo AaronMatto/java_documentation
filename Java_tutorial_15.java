9ae0f3d404b8343f9320f979b2f5d9b6656f1e1e
Aaron Matto <mattoaaron@gmail.com>
2022-05-26 20:05:55 +0100

    panels

--- Java_tutorial_15.java
+++ Java_tutorial_15.java
@@ -3,7 +3,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 
@@ -35,8 +34,8 @@ public class Java_tutorial_14 {
     greenPanel.setBounds(0, 300, 600, 300);
 
     // now we have a square that's red, blue and green! We can add components (labels) to our panels or a frame.
-    // lets add it to a panel
-    greenPanel.add(label);
+    // lets add it to a panel (although we must do this after all other customisations)
+
     // we can also move the label around within the panel. However, these panels use a 'flow' layout manager
     // Frames have a 'border' layour manager
     // A container using 'flow' layour manager takes components by default, centers them and sticks them at
@@ -45,11 +44,25 @@ public class Java_tutorial_14 {
     greenPanel.setLayout(new BorderLayout()); // for now we're going to work with border layouts
     // as in the last tutorial, this centres components vertically and sticks them to the left of their container
     // horizontally. With borderlayout we can use setVertical and setHorizontal
-    label.setVerticalAlignment(JLabel.TOP);
+    label.setVerticalAlignment(JLabel.BOTTOM);
+    label.setHorizontalAlignment(JLabel.RIGHT); // so now our label is in bottom right of green panel
 
 
+    // If we don't want to use a layout manager we can get more specific with positioning a component within
+    // a panel. First we setLayout of each panel to null:
+      // bluePanel.setLayout(null);
+      // redPanel.setLayout(null);
+      // greenPanel.setLayout(null);
+    // Now we set the bounds of the label
+      // label.setBounds(0, 0, 75, 75);
+      // Since we are adding the label to the green panel, 0,0 here references the x and y coordinates of where
+      // the top left of our label will appear within THAT (green) panel, not the frame. The panel is the container.
+      // Coordinates are relative to the top left corner of the container.
 
+    // SUMMARY
+    // so you can add components/labels to a panel then add that panel to a frame.
 
+    greenPanel.add(label);
 
     JFrame frame = new JFrame();
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -61,5 +74,4 @@ public class Java_tutorial_14 {
     frame.add(greenPanel);
 
   }
-
 }
