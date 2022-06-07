7b5979ca36f08f82bc6ea67bccd9d4146d22b41a
Aaron Matto <mattoaaron@gmail.com>
2022-06-07 17:36:12 +0100

    Jpanels, Jbuttons and events

--- Java_tutorial_15.java
+++ Java_tutorial_15.java
@@ -1,14 +1,19 @@
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-// CONTENTS: Jpanels,
+// CONTENTS: Jpanels, Jbuttons and events,
 
-public class Java_tutorial_14 {
+public class Java_tutorial_15 {
 
   public static void main(String[] args) {
 
@@ -73,5 +78,70 @@ public class Java_tutorial_14 {
     frame.add(bluePanel);
     frame.add(greenPanel);
 
+    // BUTTONS
+    new MyFrame();
   }
+}
+
+// jButtons!
+public class MyFrame extends JFrame implements ActionListener {
+  // our class MyFrame is inheriting from JFrame.
+  // Jbutton = a button that performs an action when clicked on
+  // We need to add 'implements ActionListener' to grant access to event listener functionality
+  // We declare our button in the class so it is globally scoped. We set it up in the constructor.
+  JButton button;
+  ImageIcon icon = new ImageIcon("YE.png");
+  JLabel labelTwo; // we can make this label appear when we click the button. Lines 119-122, 130 & 142!
+
+
+  MyFrame(){
+    button = new JButton();
+    button.setBounds(200,100,350,300);
+    button.addActionListener(this); //  we pass in "this" since this class is implementing the
+    // ActionListener interface.
+    button.setText("I'm a button");
+    button.setFocusable(false); // gets rid of the box around the button text
+    // button.setIcon(icon); // add an image to our button (our image is too big)
+    button.setHorizontalTextPosition(JButton.CENTER);
+    button.setVerticalTextPosition(JButton.BOTTOM);
+    button.setFont(new Font("Comic Sans", Font.BOLD, 25));
+    button.setIconTextGap(-10); // reduce space between icon and button text
+    button.setForeground(Color.cyan);
+    button.setBackground(Color.black);
+    button.setBorder(BorderFactory.createEtchedBorder());
+
+    // *** we can do this all with a giant shortcut: a lambda expression
+    // We don't need to add 'implements ActionListener' to MyFrame
+    // We don't need to create the actionPerformed method
+    // button.addActionListener(e -> System.out.println("success!"));
+    // the line above is a lambda expression!
+
+    labelTwo = new JLabel();
+    labelTwo.setIcon(icon);
+    labelTwo.setBounds(150, 250, 150, 150);
+    labelTwo.setVisible(false);
+
+    // SETTING UP the button!
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setLayout(null);
+    this.setSize(500,500);
+    this.setVisible(true);
+    this.add(button);
+    this.add(labelTwo);
+  }
+
+  // Event listener! actionPerformed is the default method name for when an event occurs hence the override
+  // as we're defining what happens here
+  // As usual, e is the event object and has multiple native methods e.g. getSource which is e.currentTarget
+  // The data type of e in java is ActionEvent!
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if(e.getSource() == button){
+      System.out.println("Success!");
+      // button.setEnabled(false); this stops the button being clickable after it has been clicked once!
+      labelTwo.setVisible(true);
+    }
+  }
+
+
 }
